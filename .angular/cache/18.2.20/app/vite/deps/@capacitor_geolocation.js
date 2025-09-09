63d323a68275e4e974f507c1170fbde2ac39b0e0
LaserEx07 <junrelejurango@gmail.com>
2025-09-09 13:09:34 +0800

    new chunck

---
+++ .angular/cache/18.2.20/app/vite/deps/@capacitor_geolocation.js
@@ -0,0 +1,17 @@
+import {
+  y
+} from "./chunk-LVBQUHDE.js";
+import {
+  registerPlugin
+} from "./chunk-MCFBZ5YE.js";
+import "./chunk-EAE2VPRF.js";
+
+// node_modules/@capacitor/geolocation/dist/esm/index.js
+var Geolocation = registerPlugin("Geolocation", {
+  web: () => import("./web-7OK4KL3X.js").then((m) => new m.GeolocationWeb())
+});
+y();
+export {
+  Geolocation
+};
+//# sourceMappingURL=@capacitor_geolocation.js.map
---
+++ .angular/cache/18.2.20/app/vite/deps/@capacitor_haptics.js
@@ -0,0 +1,19 @@
+import {
+  ImpactStyle,
+  NotificationType
+} from "./chunk-OGKSRTVA.js";
+import {
+  registerPlugin
+} from "./chunk-MCFBZ5YE.js";
+import "./chunk-EAE2VPRF.js";
+
+// node_modules/@capacitor/haptics/dist/esm/index.js
+var Haptics = registerPlugin("Haptics", {
+  web: () => import("./web-OFE4VD5L.js").then((m) => new m.HapticsWeb())
+});
+export {
+  Haptics,
+  ImpactStyle,
+  NotificationType
+};
+//# sourceMappingURL=@capacitor_haptics.js.map
