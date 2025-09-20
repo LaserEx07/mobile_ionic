53246d8fad4a70b2aba859cce3cc6506072b3835
LaserEx07 <junrelejurango@gmail.com>
2025-09-21 01:36:32 +0800

    revision change api

--- src/app/services/mobile-user.service.spec.ts
+++ src/app/services/mobile-user.service.spec.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class MobileUserService {
-  private apiUrl = 'http://192.168.112.244:8000/api/mobile-users';
+  private apiUrl = `${environment.apiUrl.replace(/\/api\/?$/, '')}/api/mobile-users`;
 
   constructor(private http: HttpClient) {}
 
